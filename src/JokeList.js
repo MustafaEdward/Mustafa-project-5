e403db2372bd8ab78df8a1780d0d981f85972dbf
Mustafa <mustafa.edward24@gmail.com>
2019-09-08 18:29:18 -0400

    setting up localStorage&React

--- src/JokeList.js
+++ src/JokeList.js
@@ -3,49 +3,67 @@ import Joke from './Joke';
 import axios from 'axios';
 import uuid from 'uuid/v4';
 import './JokeList.scss';
-import { jsxAttribute } from '@babel/types';
+
 
 
 class JokeList extends Component {
     static defaultProps = {
-       numberOfJokes : 7,
+       numberOfJokes : 10,
     };
    constructor(props){
        super(props);
        this.state = {
-           jokes : [],
+           //load 7 jokes from localStage and if nothing there ,set to an empty array.
+           jokes : JSON.parse(window.localStorage.getItem("jokes")||"[]"), // JSON.parse in order to get the data from localStorage 
+                                                                          //which is a string and turn in into an object.
        }
+       this.handleClick= this.handleClick.bind(this);
    }
 
-   async componentDidMount() {
-       //load jokes 
-       let jokesArray = [];
-     while (jokesArray.length < this.props.numberOfJokes) {
-        let res=  await axios.get("https://icanhazdadjoke.com/", 
-           {headers: {accept: "application/json"}
-    });
-     jokesArray.push({id:uuid(), text: res.data.joke, votes: 0});
-     }
-     console.log(jokesArray );
-
-     this.setState({ 
-         jokes: jokesArray
-         });
+   componentDidMount() {
+      if(this.state.jokes.length=== 0) this.getJokes();
    }
+
+  
+   async getJokes() {
+    let jokesArray = [];
+     //load jokes 
+    while (jokesArray.length < this.props.numberOfJokes) {
+       let res=  await axios.get("https://icanhazdadjoke.com/", 
+          {headers: {accept: "application/json"}
+   });
+    jokesArray.push({id:uuid(), text: res.data.joke, votes: 0});
+    }
+    console.log(jokesArray );
+
+    this.setState(currSt=> ({
+        jokes: [...currSt.jokes , ...jokesArray]
+    }),
+    ()=> window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)) // update votes in localStorage 
+    //stringify because it only accpets a string
+       
+    );
+}
+
 
    handelVote(id, updateNumber) {
        this.setState(
            prevSate => ({
                jokes: prevSate.jokes.map((singleJoke=>
                 singleJoke.id===id ? {...singleJoke, votes: singleJoke.votes + updateNumber}:singleJoke))
-           })
-       )   
+           }),
+           ()=> window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)) // update votes in localStorage
+       
+        );  
+   }
+   handleClick(){
+       this.getJokes();
    }
     render() { 
         return ( 
             <div className="JokeList">
                 <h1 jokeList-title>Safi style jokes</h1>
-                <div className="jokeList-joke">
+                <div className="JokeList-joke">
                  {this.state.jokes.map(j=> 
                  <Joke key={j.id} 
                  votes={j.votes} 
@@ -56,7 +74,7 @@ class JokeList extends Component {
                  )}
                 </div>
                 <h2 JokeList="jokeList-guide">Do you want more of these cheesy jokes?</h2>
-                <button className="jokeList-button">get more jokes</button>
+                <button className="jokeList-button" onClick={this.handleClick}>get more jokes</button>
             </div>
            );
     }
