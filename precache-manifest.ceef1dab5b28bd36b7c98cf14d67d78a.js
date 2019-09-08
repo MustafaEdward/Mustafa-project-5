223ba84f6e3d803b08c28578ffa75996dd2322c6
Mustafa <mustafa.edward24@gmail.com>
2019-09-08 12:22:37 -0400

    Updates

---
+++ precache-manifest.ceef1dab5b28bd36b7c98cf14d67d78a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39b0c01900cd19d58bc7cad9bce67a7",
+    "url": "/Mustafa-project-5/index.html"
+  },
+  {
+    "revision": "2fb506e3c32f513d6a2f",
+    "url": "/Mustafa-project-5/static/css/main.1886b260.chunk.css"
+  },
+  {
+    "revision": "d9ed685359e6c8a9b739",
+    "url": "/Mustafa-project-5/static/js/2.49831ab6.chunk.js"
+  },
+  {
+    "revision": "2fb506e3c32f513d6a2f",
+    "url": "/Mustafa-project-5/static/js/main.5157636e.chunk.js"
+  },
+  {
+    "revision": "e8c38cdd6971fb753b99",
+    "url": "/Mustafa-project-5/static/js/runtime~main.1e554178.js"
+  }
+]);
