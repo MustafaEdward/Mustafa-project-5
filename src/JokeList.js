61d62b9fa6100a7f68d5374d835ed906446c6f2a
Mustafa <mustafa.edward24@gmail.com>
2019-09-09 21:08:22 -0400

    votes sorted, duplicated jokes removed

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Joke from './Joke';
 import axios from 'axios';
-import uuid from 'uuid/v4';
+import uuid from 'uuid/v4';// generating a unique ID
 import './JokeList.scss';
 
 
@@ -15,8 +15,9 @@ class JokeList extends Component {
        this.state = {
            //load 7 jokes from localStage and if nothing there ,set to an empty array.
            jokes : JSON.parse(window.localStorage.getItem("jokes")||"[]"), // JSON.parse in order to get the data from localStorage 
-           loading: false,                                                             //which is a string and turn in into an object.
+           loading: false,                                                 //which is a string and turn in into an object.
        }
+       this.loadedJokes = new Set(this.state.jokes.map(j=>j.text)); // preventing duplicate jokes 
        this.handleClick= this.handleClick.bind(this);
    }
 
@@ -26,15 +27,21 @@ class JokeList extends Component {
 
   
    async getJokes() {
+    try {
     let jokesArray = [];
      //load jokes 
     while (jokesArray.length < this.props.numberOfJokes) {
        let res=  await axios.get("https://icanhazdadjoke.com/", 
           {headers: {accept: "application/json"}
    });
-    jokesArray.push({id:uuid(), text: res.data.joke, votes: 0});
+   let newJokes = res.data.joke;
+   if(!this.loadedJokes.has(newJokes)){
+    jokesArray.push({id:uuid(), text: newJokes, votes: 0});
+   } else {
+       console.log(newJokes);
+   }
+ 
     }
-    console.log(jokesArray );
 
     this.setState(currSt=> ({
         loading:false,
@@ -44,6 +51,12 @@ class JokeList extends Component {
     //stringify because it only accpets a string
        
     );
+  } catch(e) {
+      alert (`Sorry 😟😟😟 ${e} 😟😟😟` );
+      this.setState({
+          loading:false,
+      })
+  }
 }
    handelVote(id, updateNumber) {
        this.setState(
@@ -59,6 +72,8 @@ class JokeList extends Component {
                                                          //user get the reults after loading
        //this.getJokes();
    }
+
+   
     render() { 
         if(this.state.loading) {
             return(
@@ -68,11 +83,12 @@ class JokeList extends Component {
                 </div>
             )
         }
+        let jokes = this.state.jokes.sort((a,b)=> b.votes - a.votes); // sorting jokes based on votes 
         return ( 
             <div className="JokeList">
                 <h1 className="JokeList__title">Safi style <span className="underline--magical">jokes</span></h1>
                 <div className="JokeList__Joke">
-                 {this.state.jokes.map(j=> 
+                 {jokes.map(j=> 
                  <Joke key={j.id} 
                  votes={j.votes} 
                  text={j.text} 
